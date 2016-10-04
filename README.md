1e14d8dc2bee96f953ac2ff2071858dcac738fa2
David Bethune <dbethune1970@gmail.com>
2016-10-04 13:07:48 -0700

    all test wrote and passing

---
+++ README.md
@@ -0,0 +1,70 @@
+## Wildlife Tracker##
+October 4, 2016
+David Bethune
+dbethune1970@gmail.com
+dbethune0109@ github
+
+##specs##
+Wildlife Tracker
+The Forest Service is considering a proposal from a timber company to clearcut a nearby forest of Douglas Fir. Before this proposal may be approved, they must complete an environmental impact study. You have been asked to build an application that allows Rangers to track wildlife sightings in the area.
+
+The application must track two categories of wildlife:
+
+Animals
+
+At the very least, require:
+
+id
+name
+Endangered Animals
+
+Due to their dwindling numbers, Rangers must record additional information about EndangeredAnimals:
+
+id
+name
+health
+Use constants to define options like "healthy", "ill", and "okay".
+age (an estimated guess by the ranger)
+Use constants to define options like "newborn", "young", or "adult".
+Each time an animal species of either category is seen, a Sighting must be reported:
+
+Sightings
+
+When wildlife is spotted, a Ranger submits a form to record a Sighting containing the following:
+
+id of Animal or EndangeredAnimal species
+location
+(Conveyed in any manner you choose ie: "Zone A", "Near the River", "NE Quadrant", or latitude and longitude values are all acceptable.)
+rangerName
+
+
+
+restore databse by using psql    create database wildlife_tracker;
+create database wildlife_tracker_test with template wildlife_tracker;
+gradle run in command line.
+
+
+##specs##
+you can enter ranger name, animal type, whether it is endangered species and the location of sighting.
+ex ranger name:David, animal bald eagle, is endangered, was seen in the sky.
+
+
+
+##TECH Used##
+.gradle
+psql
+postgresql
+```html
+```css
+command line
+
+
+
+##Open Source##
+No license needed open source.
+
+
+
+
+```
+```
---
+++ src/test/java/SightingTest.java
@@ -0,0 +1,183 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+import java.util.Date;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+
+public class SightingTest{
+  Sighting testSighting;
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Before
+  public void setUp(){
+    testSighting = new Sighting("somewhere", "Milo");
+  }
+
+  @Test
+  public void sightings_instantiatesCorrectly_true() {
+    assertTrue(testSighting instanceof Sighting);
+  }
+
+  @Test
+  public void location_instantiatesCorrectly_true() {
+    assertEquals("somewhere", testSighting.getLocation());
+  }
+
+  @Test
+  public void rangername_instantiatesCorrectly_true() {
+    assertEquals("Milo", testSighting.getRangerName());
+  }
+
+  @Test
+  public void equals_returnsTrueIfPropertiesAreSame_true(){
+    Sighting testSighting2 = new Sighting("somewhere", "Milo");
+    assertTrue(testSighting.equals(testSighting2));
+  }
+
+  @Test
+  public void save_insertsSightingIntoDatabase_Sighting() {
+    testSighting.save();
+    Sighting testSighting2 = null;
+    try(Connection con = DB.sql2o.open()){
+      testSighting2 = con.createQuery("SELECT * FROM sightings WHERE location='somewhere'")
+      .executeAndFetchFirst(Sighting.class);
+    }
+    assertTrue(testSighting2.equals(testSighting));
+  }
+
+
+
+  @Test
+  public void all_returnsAllInstancesOfPerson_true() {
+    testSighting.save();
+    Sighting testSighting2 = new Sighting("nowhere", "Stephanie");
+    testSighting2.save();
+    assertEquals(true, Sighting.all().get(0).equals(testSighting));
+    assertEquals(true, Sighting.all().get(1).equals(testSighting2));
+  }
+
+  @Test
+  public void save_assignsIdToSighting() {
+    testSighting.save();
+    Sighting testSighting2 = Sighting.all().get(0);
+    assertEquals(testSighting.getId(), testSighting2.getId());
+  }
+
+  @Test
+  public void find_returnsSightingWithSameId_secondSighting() {
+    testSighting.save();
+    Sighting testSighting2 = new Sighting("nowhere", "Stephanie");
+    testSighting2.save();
+    assertEquals(Sighting.find(testSighting2.getId()), testSighting2);
+  }
+//exception
+  @Test(expected=IndexOutOfBoundsException.class)
+  public void find_throwsExceptionIfSightingNotFound() {
+    Sighting.find(1);
+  }
+
+  @Test
+  public void save_insertsCurrentDateIntoDatabase_Sighting() {
+    testSighting.save();
+    Timestamp savedDate = Sighting.find(testSighting.getId()).getDateSighted();
+    Timestamp rightNow = new Timestamp(new Date().getTime());
+    assertEquals(rightNow.getDate(), savedDate.getDate());
+    assertEquals(rightNow.getHours(), savedDate.getHours());
+  }
+
+  // @Test
+  // public void getFormattedDate_returnsFormattedDate_Sighting() {
+  //   testSighting.save();
+  //   Sighting savedSighting = Sighting.find(testSighting.getId());
+  //   Timestamp rightNow = new Timestamp(new Date().getTime());
+  //   assertEquals(DateFormat.getDateTimeInstance().format(rightNow), savedSighting.getFormattedDate());
+  // }
+
+  @Test
+  public void delete_deletesEntryInDatabase_0(){
+    testSighting.save();
+    testSighting.delete();
+    assertEquals(0, Sighting.all().size());
+  }
+
+  @Test
+  public void delete_deletesSightingAssociations(){
+    testSighting.save();
+    RegularAnimal testRegularAnimal = new RegularAnimal("Milo", "frog");
+    testRegularAnimal.save();
+    testSighting.addAnimal(testRegularAnimal);
+    testSighting.delete();
+    assertEquals(0, testRegularAnimal.getSightings().size());
+  }
+
+  @Test
+  public void addAnimal_addsRegularAnimalToSighting(){
+    testSighting.save();
+    RegularAnimal testRegularAnimal = new RegularAnimal("FLea Ridden","alligator");
+    testRegularAnimal.save();
+    testSighting.addAnimal(testRegularAnimal);
+    RegularAnimal  savedRegularAnimal = testSighting.getRegularAnimals().get(0);
+    assertTrue(testRegularAnimal.equals(savedRegularAnimal));
+  }
+
+  @Test
+  public void getRegularAnimals_returnsAllRegularAnimals_int(){
+    testSighting.save();
+    RegularAnimal testRegularAnimal = new RegularAnimal("FLea Ridden","alligator");
+    testRegularAnimal.save();
+    testSighting.addAnimal(testRegularAnimal);
+    List savedAnimals = testSighting.getRegularAnimals();
+    assertEquals(1, savedAnimals.size());
+    assertTrue(savedAnimals.contains(testRegularAnimal));
+  }
+
+  @Test
+  public void addAnimal_addsEndangeredAnimalToSighting(){
+    testSighting.save();
+    EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("FLea Ridden","potato bug", "ill", "old");
+    testEndangeredAnimal.save();
+    testSighting.addAnimal(testEndangeredAnimal);
+    EndangeredAnimal  savedEndangeredAnimal = testSighting.getEndangeredAnimals().get(0);
+    assertTrue(testEndangeredAnimal.equals(savedEndangeredAnimal));
+  }
+
+  @Test
+  public void getEndangeredAnimals_returnsAllEndangeredAnimals_int(){
+    testSighting.save();
+    EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("FLea Ridden","potato bug", "ill", "old");
+    testEndangeredAnimal.save();
+    testSighting.addAnimal(testEndangeredAnimal);
+    List savedAnimals = testSighting.getEndangeredAnimals();
+    assertEquals(1, savedAnimals.size());
+    assertTrue(savedAnimals.contains(testEndangeredAnimal));
+  }
+
+  @Test
+  public void allByDate_sortsSightingsListByMostRecent_Sighting(){
+    testSighting.save();
+    Sighting testSighting2 = new Sighting("nowhere", "Stephanie");
+    testSighting2.save();
+    assertEquals(testSighting2, Sighting.allByDate().get(0));
+  }
+
+  @Test
+  public void mostRecent_returnsFiveMostRecent_Sighting(){
+    testSighting.save();
+    Sighting testSighting2 = new Sighting("nowhere", "Stephanie");
+    testSighting2.save();
+    Sighting testSighting3 = new Sighting("nowhere", "Stephanie");
+    testSighting3.save();
+    Sighting testSighting4 = new Sighting("nowhere", "Stephanie");
+    testSighting4.save();
+    Sighting testSighting5 = new Sighting("nowhere", "Stephanie");
+    testSighting5.save();
+    Sighting testSighting6 = new Sighting("nowhere", "Stephanie");
+    testSighting6.save();
+    assertEquals(testSighting6, Sighting.mostRecent().get(0));
+    assertFalse(Sighting.mostRecent().contains(testSighting));
+  }
+  }
